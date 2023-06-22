a7fe2ff2f74a3248e4c664f29d4141529ea11d1d
JaIoI <mirzamurodov.jalol@gmail.com>
2023-06-22 23:12:29 +0500

    novelties

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(evt) { 
+document.addEventListener("DOMContentLoaded", function(evt) {
 	const swiper_banner = new Swiper('.swiper_banner', {
 		navigation: {
 			nextEl: ".swiper-button-next",
 			prevEl: ".swiper-button-prev",
 		},
 		effect: 'fade',
+	});
+
+	const swiper_novelties = new Swiper('.swiper_novelties', {
+		slidesPerView: 4,
+		navigation: {
+			nextEl: ".novelties__top .swiper-button-next",
+			prevEl: ".novelties__top .swiper-button-prev",
+		},
 	});
 })
