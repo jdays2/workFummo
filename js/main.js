64e2a0b511e2f34959ff32c9df29523fc0de71ed
JaIoI <mirzamurodov.jalol@gmail.com>
2023-07-01 21:31:41 +0500

    main page mob

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(evt) {
+document.addEventListener("DOMContentLoaded", function (evt) {
+
+	const debounced = [];
+	const cancelFunc = (timeout) => () => {
+		clearTimeout(timeout);
+	};
+	let debounce = (fn, wait, ...args) => {
+		let d = debounced.find(({ funcString }) => funcString === fn.toString());
+
+		if (d) {
+			d.cancel();
+		} else {
+			d = {};
+			debounced.push(d);
+		}
+
+		d.func = fn;
+		d.funcString = fn.toString();
+		d.timeout = setTimeout(fn, wait, ...args);
+		d.cancel = cancelFunc(d.timeout);
+	};
+
 	const swiper_banner = new Swiper('.swiper_banner', {
 		navigation: {
 			nextEl: ".swiper-button-next",
@@ -8,14 +29,19 @@ document.addEventListener("DOMContentLoaded", function(evt) {
 	});
 
 	const swiper_novelties = new Swiper('.swiper_novelties', {
-		slidesPerView: 4,
+		slidesPerView: 1.22,
 		navigation: {
 			nextEl: ".novelties__top .swiper-button-next",
 			prevEl: ".novelties__top .swiper-button-prev",
 		},
+		breakpoints: {
+			768: {
+				slidesPerView: 4,
+			}
+		}
 	});
 
-	$(document).on('click', '.novelties__filter_btn', function(evt) {
+	$(document).on('click', '.novelties__filter_btn', function (evt) {
 		evt.preventDefault();
 		let $this = $(this);
 		let thisPos = $this.position().left;
@@ -29,6 +55,36 @@ document.addEventListener("DOMContentLoaded", function(evt) {
 			left: thisPos + 'rem',
 			width: thisWidth + 'rem',
 		});
-		
-	})
+
+	});
+
+	$(document).on('input', 'textarea', function () {
+		debounce(() => {
+			this.style.height = 'auto';
+			$(this).outerHeight((this.scrollHeight) + 'px');
+		}, 100);
+	});
+
+	function headerState(documentScrollTop) {
+		if (documentScrollTop > 1) {
+			$('.header').addClass('header_min');
+		} else {
+			$('.header').removeClass('header_min');
+		}
+	}
+
+	headerState($(document).scrollTop());
+
+	$(document).on('scroll', function (evt) {
+		let thisScroll = $(this).scrollTop();
+		headerState(thisScroll);
+	});
+
+	$(document).on('click', '.burger', function (evt) {
+		evt.preventDefault();
+		$('html').toggleClass('mob_menu');
+		$(this).toggleClass('cross');
+	});
+
+
 })
